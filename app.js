e472676509a4f5cac1db1af298e2cdd9662e478a
KC-426 <kuldeepchahar426@gmail.com>
2023-09-01 18:02:15 +0530

    live

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const Admin_Routes = require("./routes/admin_routes");
 const Category_Routes = require("./routes/brands_routes");
 const Order_Routes = require("./routes/order_routes");
 const Products_Routes = require("./routes/products_routes");
+const Mobilebanners_Routes = require("./routes/Mobilebanner_routes");
 const User_Routes = require("./routes/user_routes");
 const Banners_Routes = require("./routes/banners_routes");
 const Enquiry_Routes = require("./routes/enquiry_routes");
@@ -44,6 +45,7 @@ app.use(morgan("dev"));
 app.use("/api", Admin_Routes);
 app.use("/api", Category_Routes);
 app.use("/api", Order_Routes);
+app.use('/api', Mobilebanners_Routes)
 app.use("/api", Products_Routes);
 app.use("/api", User_Routes);
 app.use("/api", Banners_Routes);
